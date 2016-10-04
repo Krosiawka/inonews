a83cf07b1489bf89323ef3fb68815fab10b52c94
Paliy Vladimir <v_paliy@inbox.ru>
2016-10-04 14:24:28 +0300

    fix bad code (orderby user list)

--- application/models/users_list_model.php
+++ application/models/users_list_model.php
@@ -13,6 +13,7 @@
 
 		public function index($id)
 		{
+			$this->db->order_by('fk_id','ASC');
 			$this->db->where('fk_id !=', $id);//вытаскиваем всех пользователей и администраторов за исключением самого инициатора запроса
             $query = $this->db->get('users');
             return $query->result_array();			
